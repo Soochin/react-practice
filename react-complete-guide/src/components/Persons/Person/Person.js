6c835df5f7268e6d81c6d31b67165e5a18cae3a6
Soochin Kang <soochink@uci.edu>
2020-09-17 15:11:33 -0700

    Added a new project template for https reqeust

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import classes from './Person.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const person = (props) => {
-    return (  
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    );
+import classes from './Person.css';
+import withClass from '../../../hoc/withClass';
+import Aux from '../../../hoc/Aux';
+import AuthContext from '../../../context/auth-context';
+
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    static contextType = AuthContext;
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+        console.log(this.context.authenticated);
+    }
+
+    render() {
+        console.log('[Person.js] rendering...');
+        return (  
+            <Aux>
+                {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input 
+                    type="text"
+                    // ref={(inputEl) => { this.inputElement = inputEl }} 
+                    ref={this.inputElementRef}
+                    onChange={this.props.changed} 
+                    value={this.props.name} />
+            </Aux>
+        );
+    }
 };
 
-export default person;
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
